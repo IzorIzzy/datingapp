0c1b3b9082fbd566957dd3059df3e203d21a5037
Tiffany <tiff.sobczak@gmail.com>
2022-02-20 18:01:33 -0600

    formatted with prettier

--- controllers/home-routes.js
+++ controllers/home-routes.js
@@ -1,29 +1,27 @@
 const router = require('express').Router();
-const sequelize=require('../config/connection');
-const {Profile}= require('../models');
+const sequelize = require('../config/connection');
+const {Profile} = require('../models');
 
 router.get('/', (req, res) => {
     Profile.findAll({
-      attributes: [
-        'id',
-        'username',
-        'bio',
-        'age',
-        'shift',
-        'contact',
-        'imageurl',
-      ],
-      
-    })
-      .then(dbProfileData => {
-        const profiles = dbProfileData.map(post => post.get({ plain: true }));
+        attributes: [
+            'id',
+            'username',
+            'bio',
+            'age',
+            'shift',
+            'contact',
+            'imageurl',
+        ]
+
+    }).then(dbProfileData => {
+        const profiles = dbProfileData.map(post => post.get({plain: true}));
         // pass a single post object into the homepage template
-        res.render('homepage', { profiles, session:req.session });
-      })
-      .catch(err => {
+        res.render('homepage', {profiles, session: req.session});
+    }).catch(err => {
         console.log(err);
         res.status(500).json(err);
-      });
-  });
+    });
+});
 
 module.exports = router;
